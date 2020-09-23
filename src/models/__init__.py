b525602db40120a892ab295ac4831326252294b1
CJLee94 <sl5954@nyu.edu>
2020-09-23 17:15:07 +0000

    add our method to PONet file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,10 +5,11 @@ import pandas as pd
 import pickle, os
 import numpy as np
 from . import base_networks
-from haven import haven_results as hr 
+# from haven import haven_results as hr 
 from haven import haven_chk as hc
 from . import lcfcn
 from . import PENet
+from . import PONet
 
 
 def get_model(model_dict, exp_dict=None, train_set=None):
@@ -16,5 +17,7 @@ def get_model(model_dict, exp_dict=None, train_set=None):
         model =  lcfcn.LCFCN(exp_dict, train_set=train_set)
     elif model_dict["name"] in ["penet"]:
         model = PENet.PENet(exp_dict, train_set =train_set)
+    elif model_dict["name"] in ["ponet"]:
+        model = PONet.PONet(exp_dict, train_set =train_set)
     return model
         
