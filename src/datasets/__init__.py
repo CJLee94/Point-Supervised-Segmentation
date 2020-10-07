f07a4c3acfef2cdee3303f923d5baeb6f2161fa4
CJLee94 <sl5954@nyu.edu>
2020-10-07 17:30:16 +0000

    reconstruct the augmentation part so that the point annotation of different kind can be augmented as well

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -15,8 +15,9 @@ class HEDataset(Dataset):
         self.data_dir = data_dir
         self.n_classes = n_classes
         self.option = option
-        self.names = glob.glob(os.path.join(self.data_dir,'**',option, "*_norm.tif"))
-        self.names = [path[:-9] for path in self.names]
+        self.files_no = len(glob.glob(os.path.join(self.data_dir,option,"Norms","*.png")))
+#         self.names = glob.glob(os.path.join(self.data_dir,'**',option, "*_norm.tif"))
+#         self.names = [path[:-9] for path in self.names]
 #         #names = list(map(str,list(np.sort(list(map(int,self.data.keys()))))))
 #         with h5py.File(self.datafp,'r') as h5f:
 #             names = np.sort(list(h5f.keys())).tolist()
@@ -33,45 +34,54 @@ class HEDataset(Dataset):
 #             point_label = All[3]
 #             bkg = All[4]
 #             mask = All[5]
-        real_ind = ind%(len(self.names))
-        image = sio.imread(self.names[real_ind]+"_norm.tif")
-        obj = sio.imread(self.names[real_ind]+"_obj.tif")
-        bkg = sio.imread(self.names[real_ind]+"_bkgw.tif")
-        mask = sio.imread(self.names[real_ind]+"_mask.tif")
-        points = hu.load_json(self.names[real_ind]+"_points.json")
+        real_ind = ind%(self.files_no)+1
+#         real_ind = ind+1
+        image = sio.imread(os.path.join(self.data_dir,self.option,"Norms",self.option.lower()+'_'+str(real_ind)+".png"))[...,:3]
+        obj = sio.imread(os.path.join(self.data_dir,self.option,"Objs",self.option.lower()+'_'+str(real_ind)+".tif"))
+        bkg = sio.imread(os.path.join(self.data_dir,self.option,"Bkgs",self.option.lower()+'_'+str(real_ind)+".tif"))
+        mask = sio.imread(os.path.join(self.data_dir,self.option,"GTs",self.option.lower()+'_'+str(real_ind)+".tif"))
+        region = sio.imread(os.path.join(self.data_dir,self.option,"Regions",self.option.lower()+'_'+str(real_ind)+".tif"))
+        points = hu.load_json(os.path.join(self.data_dir,self.option,"Pts",self.option.lower()+'_'+str(real_ind)+".json"))
 
         if self.transform:
-#             color_aug = HueSaturationValue(hue_shift_limit = 10, sat_shift_limit=15, val_shift_limit=10,p=0.3)
-#             target = {}
-#             target['image'] = 'image'
-#             target['image0'] = 'image'
-#             target['image1'] = 'image'
-#             target['image2'] = 'image'
-#             transformed = Compose(self.transform,p=1,additional_targets=target)(image=X,image0=point_label,image1 = bkg, image2 = mask)
-            transformed = self.transform(image = image, keypoints = points, mask = mask, mask0 = bkg, mask1 = obj)
-#             point_label = transformed['image0']
-#             point_label = point_label.clip(0,1).round().astype('uint8')
-#             bkg = transformed['image1']
-#             bkg = bkg.clip(0,1).round().astype('uint8')
-
-#             X = (transformed['image']*255).astype(np.uint8)
-#             X = color_aug(image = X)['image'].transpose(2,0,1)/255
-
+            transformed = self.transform(image = image, 
+                                         keypoints = points["1"], 
+                                         keypoints0=points["2"],
+                                         keypoints1=points["3"],
+                                         keypoints2=points["4"],
+                                         keypoints3=points["5"],
+                                         keypoints4=points["6"],
+                                         keypoints5=points["7"],
+                                         mask = mask, 
+                                         mask0 = bkg, 
+                                         mask1 = obj, 
+                                         mask2 = region)
             image = transformed["image"]
-            points = np.array(transformed["keypoints"]).astype(int)
+            points["1"] = np.array(transformed["keypoints"]).astype(int)
+            points["2"] = np.array(transformed["keypoints0"]).astype(int)
+            points["3"] = np.array(transformed["keypoints1"]).astype(int)
+            points["4"] = np.array(transformed["keypoints2"]).astype(int)
+            points["5"] = np.array(transformed["keypoints3"]).astype(int)
+            points["6"] = np.array(transformed["keypoints4"]).astype(int)
+            points["7"] = np.array(transformed["keypoints5"]).astype(int)
             mask = transformed["mask"]
             bkg = transformed["mask0"]
             obj = transformed["mask1"]
-            counts = len(points)
+            region = transformed["mask2"]
+#             counts = len(points)
             
             point_label = np.zeros_like(mask)
-            if counts>0:
-                point_label[points[:,1], points[:,0]] = 1
+            counts = 0
+            for k,v in points.items():
+                counts += len(v)
+                if len(v)>0:
+                    point_label[v[:,1], v[:,0]] = int(k)
             return {'images':torch.FloatTensor(image.transpose(2,0,1))/255.0,
                     'points':torch.FloatTensor(point_label),
                     'bkg': torch.FloatTensor(bkg),
                     'obj': torch.FloatTensor(obj),
                     'gt':torch.FloatTensor(mask),
+                    'region':torch.FloatTensor(region),
                     'counts': counts,
                     'meta':{'index':ind}}
         else:
@@ -85,7 +95,7 @@ class HEDataset(Dataset):
     
     def __len__(self):
         if self.option is "Train":
-            return 4000*len(self.names)
+            return 500*self.files_no
         else:
-            return len(self.names)
+            return self.files_no
 7
