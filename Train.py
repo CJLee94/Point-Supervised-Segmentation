2c4fc1b36a4943ea118f6566c07649f8d9bbbf11
CJLee94 <sl5954@nyu.edu>
2020-10-14 15:44:32 -0700

    add argparse and shutil to make it a executable script

--- Train.py
+++ Train.py
@@ -2,18 +2,31 @@
 import os
 from trainval import trainval
 from haven import haven_utils as hu
+import shutil
+import argparse
 # import matplotlib.pyplot as plt
 # %matplotlib inline
 
 # Define the dataset you want to train on, you can type 'TNBC' or 'MoNuSegTrainingData'
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-e', '--exp_dict', required=True, type=str)
+
+args = parser.parse_args()
+
 datadir = './CoNSeP/' 
 
-exp_dict = hu.load_json('exp_config_ponet.json')
+# exp_dict_file = 'exp_config_ponet.json'
+# exp_dict_file = args.exp_dict
+
+exp_dict = hu.load_json(args.exp_dict)
 savedir_base = "./Result"
 os.makedirs(savedir_base, exist_ok=True)
 trainval(exp_dict,
          savedir_base,
          datadir,
-         reset=True,
+         reset=False,
          num_workers=25
          )
+shutil.copy(args.exp_dict, os.path.join(savedir_base, hu.hash_dict(exp_dict), args.exp_dict))
